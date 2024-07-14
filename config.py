3fddaffaa6aa4d225ae83b891cd62226e1efda57
Harshit-2005 <168950439+harshit-2008@users.noreply.github.com>
2024-07-14 19:11:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 api_id = 27190467
 
-api_hash = "9ff6bc6ad2faba520f426cf04ca7f5773"
+api_hash = "ff6bc6ad2faba520f426cf04ca7f5773"
 
-bot_token = "7101306689:AAGEoZ0P7o4bM0I8J1CTvFQQ4lqBlHpiGlg"
+bot_token = "7101306689:AAEocjX3ESu1TbwZcdY27Eod4CuJOdJaTio"
 
-GROUP_TAG = "harshitrips"
+GROUP_TAG = "SharkToonsIndia"
 
 sudo_users = [7102604217,6224047735,6224047735,5844217074,5574593875,6111379176,898535201,5952211420,6066102279]
 
